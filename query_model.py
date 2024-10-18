4922fa57ba8d761162442f7de4fc166bdb5e4ea1
ym496 <yogendramishra2004@gmail.com>
2024-10-18 05:54:08 +0530

    Add transcripts + Update model

--- query_model.py
+++ query_model.py
@@ -9,9 +9,28 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 genai.configure(api_key=api_key)
 
+prompt = """ 
 
-query = "what's the purpose of life? how to find a purpose?"
-prompt = query
-model = genai.GenerativeModel('gemini-1.5-pro-latest')
-response = model.generate_content(prompt)
+This a transcript of one the episode of the show, Better Call Saul. I want you to list down all the idioms they've used in a JSON format.
+
+Only include idioms that were spoken by the character. Ignore it, if it's a descriptive text.
+
+Use this JSON schema:
+{
+  "episode_title": "string",
+  "episode_number": "integer",
+  "idioms": [
+    {
+      "text": "string",
+      "definition": "string",
+      "usage in show": "string",
+    }
+  ]
+}
+
+"""
+model = genai.GenerativeModel("gemini-1.5-flash")
+ep_file = genai.upload_file("./ep-trans/S1ep01.txt")
+response = model.generate_content([prompt, ep_file])
+print(response.usage_metadata)
 print(response.text)
--- scrape.py
+++ scrape.py
@@ -2,11 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 base = 'https://transcripts.foreverdreaming.org/'
-response = requests.get(f"{base}viewforum.php?f=205")
+response = requests.get(f'{base}viewforum.php?f=205')
 soup = BeautifulSoup(response.content, 'html.parser')
 
 li_tags = soup.find_all('li', class_='row')
 for li in li_tags[1:]:
     ep = li.find('a',class_='topictitle')
     title = ep.text
-    link = ep['href']
+    if int(title[1]) == 0:
+        continue
+    file_name = f'S{title[1]}ep{title[3:5]}'
+    link = ep['href'][1:]
+    ep_scrape = requests.get(f'{base}{link}')
+    ep_soup = BeautifulSoup(ep_scrape.content,'html.parser')
+    content = ep_soup.find('div',class_='content')
+    with open(f'./ep-trans/{file_name}.txt','w') as file:
+        file.write(title+'\n')
+        file.write(str(content))
